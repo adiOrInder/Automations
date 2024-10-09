9e409315b37fa9aa8a3eff8b3c3d54a95d72b1e3
adi <adith.prakasinder@gmail.com>
2024-10-09 22:35:11 +0530

    Changes to be committed: 	new file:   fileTypeOrg.c

---
+++ fileTypeOrg.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+void moveFile(const char *source, const char *destination) {
+    FILE *srcFile, *destFile;
+    char ch;
+
+    srcFile = fopen(source, "r");
+    if (srcFile == NULL) {
+        printf("Cannot open source file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    destFile = fopen(destination, "w");
+    if (destFile == NULL) {
+        fclose(srcFile);
+        printf("Cannot open destination file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((ch = fgetc(srcFile)) != EOF) {
+        fputc(ch, destFile);
+    }
+
+    fclose(srcFile);
+    fclose(destFile);
+
+    if (remove(source) != 0) {
+        printf("Error deleting source file.\n");
+    } else {
+        printf("File moved successfully.\n");
+    }
+}
+
+int main() {
+    struct dirent *de;
+    DIR *dr = opendir("../CS50");
+
+    if (dr == NULL) {
+        printf("Could not open directory");
+        return 0;
+    }
+    char destination[128];  // Adjust the size based on your needs
+    char res[128];  // This should be a character array
+
+    strcpy(res, "../CS50/.vscode/");
+
+    while ((de = readdir(dr)) != NULL) {
+        if (strstr(de->d_name, ".cpp")) {
+            snprintf(destination, sizeof(destination), "%s%s", res, de->d_name);
+            moveFile(de->d_name, destination);
+            printf("Moved %s to %s\n", de->d_name, destination);
+        }
+    }
+
+    closedir(dr);
+    return 0;
+}
