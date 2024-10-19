0ed128e4dc0e3e3816d462c26d7f5ea656f29d44
adi <adith.prakasinder@gmail.com>
2024-10-19 22:17:19 +0530

    Multiple Sorting Added Version1.1 made a separate for function for ease.

--- fileTypeOrg.c
+++ fileTypeOrg.c
@@ -4,23 +4,27 @@
 #include <string.h>
 #include <sys/stat.h>
 
+// Function to move files
 void moveFile(const char *source, const char *destination) {
     FILE *srcFile, *destFile;
     char ch;
 
+    // Open source file for reading
     srcFile = fopen(source, "r");
     if (srcFile == NULL) {
-        printf("Cannot open source file.\n");
+        printf("Cannot open source file: %s\n", source);
         exit(EXIT_FAILURE);
     }
 
+    // Open destination file for writing
     destFile = fopen(destination, "w");
     if (destFile == NULL) {
         fclose(srcFile);
-        printf("Cannot open destination file.\n");
+        printf("Cannot open destination file: %s\n", destination);
         exit(EXIT_FAILURE);
     }
 
+    // Copy contents from source to destination
     while ((ch = fgetc(srcFile)) != EOF) {
         fputc(ch, destFile);
     }
@@ -28,54 +32,59 @@ void moveFile(const char *source, const char *destination) {
     fclose(srcFile);
     fclose(destFile);
 
+    // Remove the source file
     if (remove(source) != 0) {
-        printf("Error deleting source file.\n");
+        printf("Error deleting source file: %s\n", source);
     } else {
-        printf("File moved successfully.\n");
+        printf("File moved successfully: %s\n", destination);
     }
 }
 
-int main() {
-    struct dirent *de;
-    char dir[64] = "C:/Users/all/Downloads/";
-    DIR *dr = opendir(dir);
+// Function to create a directory if it doesn't exist
+void createDir(const char *dir) {
+    struct stat st = {0};
+    if (stat(dir, &st) == -1) {
+        mkdir(dir);
+    }
+}
 
+// Function to move files based on their extension
+void moveFilesByExtension(const char *dir, const char *ext, const char *subDir) {
+    DIR *dr = opendir(dir);
     if (dr == NULL) {
-        printf("Could not open directory\n");
-        return 0;
+        printf("Could not open directory: %s\n", dir);
+        exit(EXIT_FAILURE);
     }
 
+    struct dirent *de;
+    char execDir[128];
+    snprintf(execDir, sizeof(execDir), "%s/%s", dir, subDir);
+    createDir(execDir);
+
     while ((de = readdir(dr)) != NULL) {
-        if (strstr(de->d_name, ".exe")) {
-            //.exe
-            char execDirExe[64];
-            snprintf(execDirExe, sizeof(execDirExe), "%sExecutables", dir);
-            mkdir(execDirExe);
-            char source[128];
-            snprintf(source, sizeof(source), "%s%s", dir, de->d_name);
-            
-            char destination[128];
-            snprintf(destination, sizeof(destination), "%sExecutables/%s", dir, de->d_name);
-            
-            moveFile(source, destination);
-            printf("Moved %s to %s\n", de->d_name, destination);
-        }
-        else if(strstr(de->d_name,".png")||strstr(de->d_name,".jpg")||strstr(de->d_name,".jpeg")){
-            //.png/.jpg
-            char execDirImg[64];
-            snprintf(execDirImg, sizeof(execDirImg), "%sImages", dir);
-            mkdir(execDirImg);
-            char source[128];
-            snprintf(source, sizeof(source), "%s%s", dir, de->d_name);
-            
-            char destination[128];
-            snprintf(destination, sizeof(destination), "%sImages/%s", dir, de->d_name);
-            
+        if (strstr(de->d_name, ext)) {
+            char source[256];
+            snprintf(source, sizeof(source), "%s/%s", dir, de->d_name);
+            char destination[256];
+            snprintf(destination, sizeof(destination), "%s/%s", execDir, de->d_name);
             moveFile(source, destination);
             printf("Moved %s to %s\n", de->d_name, destination);
         }
     }
 
     closedir(dr);
+}
+
+int main() {
+    char dir[] = "C:/Users/all/Downloads";
+
+    // Move executable files
+    moveFilesByExtension(dir, ".exe", "Executables");
+
+    // Move image files
+    moveFilesByExtension(dir, ".png", "Images");
+    moveFilesByExtension(dir, ".jpg", "Images");
+    moveFilesByExtension(dir, ".jpeg", "Images");
+
     return 0;
 }
