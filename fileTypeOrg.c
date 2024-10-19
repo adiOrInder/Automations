99fb2ce07cf5f161daad6575eb9fb59174e828d1
adi <adith.prakasinder@gmail.com>
2024-10-19 21:38:23 +0530

    Image Sorting has been added

--- fileTypeOrg.c
+++ fileTypeOrg.c
@@ -37,24 +37,39 @@ void moveFile(const char *source, const char *destination) {
 
 int main() {
     struct dirent *de;
-    char dir[40] = "C:/Users/all/Downloads/";
+    char dir[64] = "C:/Users/all/Downloads/";
     DIR *dr = opendir(dir);
 
     if (dr == NULL) {
         printf("Could not open directory\n");
         return 0;
     }
-    char execDir[128];
-    snprintf(execDir, sizeof(execDir), "%sExecutables", dir);
-    mkdir(execDir);
-    
+
     while ((de = readdir(dr)) != NULL) {
         if (strstr(de->d_name, ".exe")) {
+            //.exe
+            char execDirExe[64];
+            snprintf(execDirExe, sizeof(execDirExe), "%sExecutables", dir);
+            mkdir(execDirExe);
             char source[128];
             snprintf(source, sizeof(source), "%s%s", dir, de->d_name);
             
             char destination[128];
             snprintf(destination, sizeof(destination), "%sExecutables/%s", dir, de->d_name);
+            
+            moveFile(source, destination);
+            printf("Moved %s to %s\n", de->d_name, destination);
+        }
+        else if(strstr(de->d_name,".png")||strstr(de->d_name,".jpg")||strstr(de->d_name,".jpeg")){
+            //.png/.jpg
+            char execDirImg[64];
+            snprintf(execDirImg, sizeof(execDirImg), "%sImages", dir);
+            mkdir(execDirImg);
+            char source[128];
+            snprintf(source, sizeof(source), "%s%s", dir, de->d_name);
+            
+            char destination[128];
+            snprintf(destination, sizeof(destination), "%sImages/%s", dir, de->d_name);
             
             moveFile(source, destination);
             printf("Moved %s to %s\n", de->d_name, destination);
